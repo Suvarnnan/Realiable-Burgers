ea74f5ee8a1f079b9ad8bba6cf70c0153ecef431
TET <nv.survarnnan@siemens-healthineers.com>
2018-05-29 14:19:27 +0530

    Initial Commit

---
+++ Realiable Burgers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Realiable Burgers
+//
+//  Created by Technology Evaluation Team on 29/05/18.
+//  Copyright © 2018 Technology Evaluation Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Realiable BurgersTests/Realiable_BurgersTests.swift
@@ -0,0 +1,36 @@
+//
+//  Realiable_BurgersTests.swift
+//  Realiable BurgersTests
+//
+//  Created by Technology Evaluation Team on 29/05/18.
+//  Copyright © 2018 Technology Evaluation Team. All rights reserved.
+//
+
+import XCTest
+@testable import Realiable_Burgers
+
+class Realiable_BurgersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
