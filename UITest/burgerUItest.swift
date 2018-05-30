1124e2fc995cae9e9e87cca36004cb57d0218318
Suv <suvarnnanvasudev@yahoo.com>
2018-05-30 16:01:10 +0530

    Create burgerUItest.swift

---
+++ UITest/burgerUItest.swift
@@ -0,0 +1,23 @@
+import XCTest
+
+class sampleapp_ios_swiftUITests: XCTestCase {
+    let app = XCUIApplication()
+    override func setUp() {
+        super.setUp()
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        app.launch()
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testSwipeUntilCrashesPage() {
+        // Assert that the crash button exists
+        
+        
+        XCTAssert(true))
+    }
+}
